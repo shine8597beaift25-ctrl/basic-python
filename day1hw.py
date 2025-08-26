6f917c96deb006fa73eba479eb378add26fe02f2
Shine <shine8597.beaift25@chitkara.edu.in>
2025-08-27 00:22:27 +0530

    Add files via upload

---
+++ day1hw.py
@@ -0,0 +1,47 @@
+a=23
+b=60
+c=a+b
+print(c)
+
+
+a=43.23
+b=98.45
+c=a+b
+print(c)
+
+
+a=562345815555
+b=451236789565
+c=a+b
+print(c)
+
+
+
+a=9
+b=2
+c=a+b
+print(c)
+
+
+A = int(input('enter 1st number'))
+B = int(input('enter 2nd number'))
+C = A+B
+print(C)
+
+
+D = float(input('enter 1st number'))
+E = float(input('enter 2nd number')) 
+F = D+E
+print(F)
+
+
+G = str(input('enter 1st number'))
+H = str(input('enter 2nd number'))
+I = G+H
+print(I)
+
+
+J = chr(input("enter 1st number"))
+k = chr(input("enter 2nd number"))
+L = J+k
+print(L)
