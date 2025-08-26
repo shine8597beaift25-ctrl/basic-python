5de68753b6a990b86cc5fbdc06a3b5f421249b03
Shine <shine8597.beaift25@chitkara.edu.in>
2025-08-26 10:44:23 +0530

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,47 @@
+#natural numbers  
+# for i in range(1,11):
+#     print(" Natural Numbers :",i)
+
+#fibonacci series
+# n = int(input("Enter number of terms: "))
+# a, b = 0, 1
+#print('Fibonacci series:')
+# for i in range(n):
+#     print(a)
+#     a, b = b, a + b
+                                                                
+#table of 5
+# for i in range(1,11):
+#     print(5,'*',i ,'=', 5*i)
+
+#even odd number
+# num = int(input('Enter a number from 0-100:'))
+# if num % 2 == 0:
+#     print('the number is even')
+# elif num  % 2 != 0:
+#     print('the number is odd ')
+# else:
+#     print('not exist!')
+
+#factorial
+# num = int(input("Enter a number:"))
+# fact = 1
+# for i in range(1,num+1):
+#     fact = fact*i
+# print("factorial of",num,"is:",fact)
+
+#sum of natural numbers
+# n = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1, n + 1):
+#     sum += i
+# print("The sum of first", n, "natural numbers is:", sum)
+
+#square of numbers
+# for i in range(1,11):
+#     print(f"{i}-{i*i}")
+
+#reverse of numbers
+n = int(input("enter a number:"))
+for i in range(n,0,-1):
+    print("numbers",i)
