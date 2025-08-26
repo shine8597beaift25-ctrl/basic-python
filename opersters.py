4f8a43f25c687ed084dc598ba7b74c5269871eda
Shine <shine8597.beaift25@chitkara.edu.in>
2025-08-26 10:44:01 +0530

    Add files via upload

---
+++ opersters.py
@@ -0,0 +1,42 @@
+#Arithmetic operators
+# a = 30
+# b = 29
+# c = a+b
+# print("the sum is", c)
+
+# d = 14.6
+# e = 9.3
+# f = d-e
+# print("the subtaction is", f)
+
+# g = 6.072
+# h = 9.8
+# i = g*h
+# print("the multiplication is", i)
+
+# j = 12
+# k = 3
+# l = j%k
+# print("the modulus is",l)
+
+# m = 14
+# n = 6
+# o = m/n
+# print("the divison is",o)
+
+
+#Assignment operators
+# p = 19
+# q = 54
+# print("p is equal to q",p==q)
+
+#Logical operators
+# s = 33
+# k = 19
+# print("s is greater than k and s is equals to k",s>k and s==k )
+# print("s is greater than k and s is equals to k",s>k or s==k)
+  
+
+y = 100
+if not y>300:
+    print("y is not greater than 300")
