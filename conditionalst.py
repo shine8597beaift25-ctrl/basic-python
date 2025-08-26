dd52e49a8f05e9e24bd0a1683e26cea717014363
Shine <shine8597.beaift25@chitkara.edu.in>
2025-08-27 00:21:40 +0530

    Add files via upload

---
+++ conditionalst.py
@@ -0,0 +1,16 @@
+#Conditional statement
+a=int(input("Enyer your age"))
+if a>18:
+    print("Eligible to vote")
+elif a==18:
+    print("Wait for some time")
+else:
+    print("Not Eligible to vote")
+
+
+
+#DIFFERENCE
+a = int(input("Enter 1st Number"))
+b = int(input("Enter 2nd Number"))
+c = a-b
+print("Difference",c)
