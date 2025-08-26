ebdf992fe1b0cb64affa785d91b2885995ea2a5e
Shine <shine8597.beaift25@chitkara.edu.in>
2025-08-27 00:23:09 +0530

    Add files via upload

---
+++ evenno.py
@@ -0,0 +1,8 @@
+#even odd number
+# num = int(input('Enter a number from 0-100:'))
+# if num % 2 == 0:
+#     print('the number is even')
+# elif num  % 2 != 0:
+#     print('the number is odd ')
+# else:
+#     print('not exist!')
