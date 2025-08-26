861a5bfb0407afb3b0826c4659fcd541308c4f26
Shine <shine8597.beaift25@chitkara.edu.in>
2025-08-27 00:26:22 +0530

    Add files via upload

---
+++ quotient&rem.py
@@ -0,0 +1,5 @@
+# Write a program to find the remainder and quotient when one number is divided by another
+s=int(input("Enter a number:"))
+n=int(input("Enter a number:"))
+print("Remainder:",s%n)
+print("Quotient:",s/n)
