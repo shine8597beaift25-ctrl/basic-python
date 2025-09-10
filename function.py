0623e8272e812cf6ea65f880904db0990b2dad97
Shine <shine8597.beaift25@chitkara.edu.in>
2025-09-10 19:32:02 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,22 @@
+# A function is a block of code that performs a specific task:
+#helps in code reusablity
+
+
+# def function_name(paramter/arguments):
+    #function body
+    #return value
+# number=[20,30,40]
+# print(len(number))
+# print(max(number))
+# print(min(number))
+# print(sum(number))
+
+# def square(x):
+#     return x*x
+# print(square(5))   #calling function
+
+m=int(input("enter a number:"))
+n=int(input("enter a number:"))   
+def add(m,n):
+    return (m+n)
+print(add,m+n)
