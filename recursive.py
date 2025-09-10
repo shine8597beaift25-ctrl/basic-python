b74d85e9cf46b29ce9e1b28a8fda99d2cd526694
Shine <shine8597.beaift25@chitkara.edu.in>
2025-09-10 19:33:13 +0530

    Add files via upload

---
+++ recursive.py
@@ -0,0 +1,15 @@
+# def factorial(n):
+#     if n==0 or n==1:
+#         return 1
+#     return n* factorial(n-1)
+# print("Factorial of 23", factorial(23))
+
+# #Yield: yield is like return, but instead of returning once, it returns a sequence of values one by one
+
+# def simple_generator():
+#     yield 1
+#     yield 2
+#     yield 3
+#     for value in simple_generator():
+#         print(value)
+    
