b3b31bd64e24242d46f3824a4848dd2c29ccb929
Shine <shine8597.beaift25@chitkara.edu.in>
2025-11-24 23:26:54 +0530

    Add files via upload

---
+++ classdiscount.py
@@ -0,0 +1,18 @@
+#q1 Product Discount System
+# Create a class Product that stores:
+# product name
+# actual price
+# discount percentage
+# Task:
+# Make a method that returns the final price after discount.
+# class Product:
+#     def __init__(self,prodname,actual_price,discount_percentage):
+#         self.prodname=prodname
+#         self.actual_price=actual_price
+#         self.discount_percentage=discount_percentage
+#     def discount(self):
+#         return self.actual_price*self.discount_percentage/100
+# p1=Product("Printer",1000,10)
+# p2=Product("Laptop",50000,5)
+# print("Product name:",p1.prodname,"Actual_Price:",p1.actual_price,"Discount:",p1.discount_percentage,"Discounted Price: ",p1.discount())
+# print("Product name:",p2.prodname,"Actual_Price:",p2.actual_price,"Discount:",p2.discount_percentage,"Discounted Price: ",p2.discount())
