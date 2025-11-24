2eeaa6602cf07faf038f5f02a15f9eb378ce3b89
Shine <shine8597.beaift25@chitkara.edu.in>
2025-11-24 23:27:32 +0530

    Add files via upload

---
+++ classdistance.py
@@ -0,0 +1,20 @@
+#q4 Distance Converter
+# Create a class Distance storing a value in kilometers.
+# Task:
+# Write methods to convert it to:
+# meters
+# centimeters
+# millimeters
+# class Distance:
+#     def __init__(self,kilometer):
+#         self.kilometer=kilometer
+#     def converter(self):
+#        meter=self.kilometer*1000
+#        centimeter=meter*100
+#        milimeter=meter*1000
+#        return meter,centimeter,milimeter
+# unit1=Distance(8)
+# m, cm, mm = unit1.converter()   
+# print("Meters:", m)
+# print("Centimeters:", cm)
+# print("Millimeters:", mm)
