e87ab635dfbe2826eefd64c98649a9bc38873a03
Shine <shine8597.beaift25@chitkara.edu.in>
2025-11-24 23:27:50 +0530

    Add files via upload

---
+++ classstudent.py
@@ -0,0 +1,20 @@
+#q2 Student Result Calculator
+# Create a class Student with:
+# name
+# marks in 3 subjects (given via constructor)
+# Task:
+# Write a method to calculate total marks, percentage, and return Pass/Fail
+# (Pass → percentage ≥ 40)
+# class Student:
+#     def __init__(self,name,Marks1,Marks2,Marks3):
+#         self.name=name
+#         self.Marks1=Marks1
+#         self.Marks2=Marks2
+#         self.Marks3=Marks3
+#     def Result(self):
+#         total=self.Marks1+self.Marks2+self.Marks3
+#         percentage=total/3
+#         if percentage>=40:
+#             grade="Pass"
+#         else:
+#            grade="Fail"
